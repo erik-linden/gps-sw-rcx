6bb74269a4e71a987b91fb14cc9aa9c11f7782b5
Erik Lindén <erik.linden42@gmail.com>
2012-07-19 16:46:21 +0200

    Made the LS solver work with the command interface.
    GLOBAL_LS_SOL now handles iterations automatically and is called from
    GPS_SW_RCX. Old solver avilable as legacy.

--- GLOBAL_LS_SOL.m
+++ GLOBAL_LS_SOL.m
@@ -1,55 +1,78 @@
+function GLOBAL_LS_SOL(sv,settings)
 
-% sv = [2 5 10 12 21 23 25 29 30 31];%
-sv = [5 7 8 9 11 15 17 18 19 26 27 28];%
-% sv = [2 5 6 10 12 13 16 21 23 25 29 30 31];
-% sv = [3 5 7 8 15 18 19 21 26 28];
-% sv = [2 5 10 12 21 23 25 29 30 31];
-r = 100;
+% Standard deviations for different residuals.
+rangeStd    = 10;  % Range std in meters
+velStd      = 0.2; % Doppler std in m/s
+accStd      = 1.0; % Acceleration magnitude std in m/s^2
+jerkStd     = 7e2; % Jerk std in m/s^3
+cJerkStd    = inf; % Clock jerk in m/s^3
+wuaStd      = 0.2/5;% Wind-up acc. in m/s^2
 
-w = [1/10,1/0.2,1/1,1e-2].^2;
-
-[x time]=nav_residuals('Init',sv,100,[w(1:3),w(4)],[0,0,0]);
-lb = -inf(size(x));
-lb(end-5:end-3) = 0.9;
-lb(end-2:end)   = -1;
-
-ub = +inf(size(x));
-ub(end-5:end-3) = 1.1;
-ub(end-2:end)   = +1;
-
+% Set lsqnonlin settings.
 options = optimset('Jacobian','on',...
-    'TolX',1e-8,...
-    'TolFun',1e-8,...
-    'MaxIter',160);
-x = lsqnonlin(@(x) nav_residuals('Func',x),x,lb,ub,options);
-% nav_residuals('turnOn',[1,0,0]);
-% [x,~,residual] = lsqnonlin(@(x) nav_residuals('Func',x),x,lb,ub,options);
-nav_residuals('turnOn',[1,0,0]);
-[x,~,residual] = lsqnonlin(@(x) nav_residuals('Func',x),x,lb,ub,options);
-% nav_residuals('turnOn',[1,1,1]);
-% x = lsqnonlin(@(x) nav_residuals('Func',x),x,lb,ub,options);
-nav_residuals('Func',x)
+        'TolX',1e-8,...
+        'TolFun',1e-8,...
+        'MaxIter',160,...
+        'Display','notify');
 
+% Give the user a hint about the weighting scheme.
+fprintf(['\nStandard deviations are:\nRange \t\t%3.G m\nVel \t\t%3.G m/s\n'...
+    'Acc \t\t%3.G m/s^2\nJerk \t\t%3.G m/s^3\nClock jerk \t%3.G m/s^3\nWind-up acc %3.G m/s^2\n'],...
+    rangeStd,velStd,accStd,jerkStd,cJerkStd,wuaStd)
+disp('These values are hard coded in GLOBAL_LS_SOL and there is no reason to assume that they are good.')
 
-[x2 time2]=nav_residuals('Init',sv,r,w,[1,0,0]);
+nSteps = size(settings,1);
+fprintf('\nSolving in %u steps.\n',nSteps)
 
-N1 = length(time);
-N2 = length(time2);
+% Weight vector, inverse varaince.
+w = 1./[rangeStd, velStd, accStd, jerkStd, cJerkStd, wuaStd].^2;
+x_old = [];
 
-for i = 1:4
-    x2(N2*(i-1)+(1:N2)) = interp1(time,x(N1*(i-1)+(1:N1)),time2,'linear','extrap');
+for n = 1:nSteps
+    r = settings(n,1);
+    enable = settings(n,2:4);
+    
+    fprintf('\nInitializing at %u Hz with switch [Vel:%u Acc:%u Jerk:%u]\n',1000/r,enable)
+    [x time]=nav_residuals('Init',sv,r,w,enable);
+    N = length(time);
+    
+    % Upper and lower bounds for acc. bias and slope.
+    lb = -inf(size(x));
+    lb(4*N+(1:3)) = 0.95;
+    lb(4*N+(4:6)) = -1;
+
+    ub = +inf(size(x));
+    ub(4*N+(1:3)) = 1.05;
+    ub(4*N+(4:6)) = +1;
+    
+    % Interpolate from old solution, if avialable.
+    if(~isempty(x_old))
+        fprintf('\nInterpolating old solution.\n')
+        
+        N_old = length(time_old);
+        
+        for i = 1:4
+            % Interpolate the 3 position varaibles and the one clock error variable
+            x(N*(i-1)+(1:N)) = ...
+                interp1(time_old,x_old(N_old*(i-1)+(1:N_old)),time,'linear','extrap');
+        end
+        % Interpolate 6 acc. bias variables.
+        x(N*4+(1:6)) = x_old(N_old*4+(1:6));
+        % Interpolate the wind-up.
+        x(N*4+6+(1:N-1)) = ...
+            interp1(time_old(1:end-1),x_old(N_old*4+6+(1:N_old-1)),time(1:end-1),'linear','extrap');
+        
+    end
+    
+    fprintf('\nSolver started.\n')
+    x = lsqnonlin(@(x) nav_residuals('Func',x),x,lb,ub,options);
+    
+    % Store new solution.
+    x_old = x;
+    time_old = time;
+    
 end
- x2(N2*4+6+(1:N2-1)) = interp1(time(1:end-1),x(N1*4+6+(1:N1-1)),time2(1:end-1),'linear','extrap');
 
-lb = -inf(size(x2));
-lb(4*N2+(1:3)) = 0.9;
-lb(4*N2+(4:6)) = -3;
-
-ub = +inf(size(x2));
-ub(4*N2+(1:3)) = 1.1;
-ub(4*N2+(4:6)) = +3;
-
-[x2,~,residual] = lsqnonlin(@(x) nav_residuals('Func',x),x2,lb,ub,options);
-% nav_residuals('turnOn',[1,1,1]);
-% [x2,~,residual] = lsqnonlin(@(x) nav_residuals('Func',x),x2,lb,ub,options);
-nav_residuals('Func',x2)
+% Save solution.
+fprintf('\nSaving solution\n')
+nav_residuals('Func',x)
--- GPS_SW_RCX.m
+++ GPS_SW_RCX.m
@@ -5,94 +5,125 @@ CONSTANTS_H;
 CONSTANTS;
 
 choose = 0;
-while(choose ~= 1 && choose ~= 2 && choose ~= 3)
-    fprintf('Would you like to:\n1) Track 1 or more satellites\n2) Obtain the navigation solution\n3) Quit\n')
+while(choose>4 || choose<1)
+    fprintf('Would you like to:\n1) Track 1 or more satellites\n2) Obtain LS navigation solution\n3) Obtain legacy navigation solution\n4) Quit\n')
     choose = input(': ');
 end
 
-if(choose == 3)
-    return;
-elseif(choose == 1)
-    prnFlag = 1;
-    while(prnFlag)
-        prn = input('\nPlease enter one or more satellites to track in the form [SV1 SV2 ...]\n  or press enter to search for all satellites: ');
-        if(isempty(prn))
-            prn = 1:32;
-            prnFlag =0;
-        else
-            prnFlag = 0;
-        end
-    end
-    
-    %the number of seconds to track the signal for
-    Nfiles = input('\nEnter the number of seconds to track the signal for: ');
-    
-    fileNo = input('\nEnter which file to start with: ');fprintf('\n');
-    
-    tStart = tic;
-    for x=1:length(prn)
-        % Start by loading the first file.
-        in_sig = LOAD_GPS_DATA(FILE_NAME,fileNo);
-        
-        % Set the time offset of the file. Assume all files have the same
-        % length.
-        t0 = fileNo*length(in_sig)*TP;
-        
-        % Figure out where to place the center frequency. This is used for
-        % acquisition.
-        if USE_AIDING
-            [df, elev] = ESTIMATE_DOPPLER(t0, prn(x));
-            dLO = GET_REC_DATA(t0, 'dFre');
-            F0 = df - dLO;
-            string = sprintf(' Elevation: %+4.f, ',elev);
-        else
-            F0 = 0;
-            elev = [];
-            string = '';
-        end
-        
-        % Do aquisition. The coherent integration time is fixed to 20
-        % milliseconds.
-        if ~USE_AIDING || elev>-20
-            code = CACODEGN(prn(x));
-            corr_aq=DIGITIZE_CA(code,0,N_CODES_AQU*ONE_MSEC_SAM);
-            [df, cst_k, magnitude] = FFT_ACQUISITION(in_sig, corr_aq, F0);
-            
-            % If the signal was not found, quit this satellite.
-            if(CNO(magnitude,N_CODES_AQU*1e-3) < DB_DETECTION)
-                fprintf('PRN %02d NOT FOUND%s CNo: %2.2f\n', prn(x), string, CNO(magnitude,N_CODES_AQU*1e-3));
-                % Otherwise, track the satellite.
+switch choose
+    case  1
+        prnFlag = 1;
+        while(prnFlag)
+            prn = input('\nPlease enter one or more satellites to track in the form [SV1 SV2 ...]\n or press enter to search for all satellites: ');
+            if(isempty(prn))
+                prn = 1:32;
+                prnFlag =0;
             else
-                fprintf('PRN %02d FOUND    %s CNo: %2.2f, Doppler Frequency: %+3.f\n', prn(x), string, CNO(magnitude,N_CODES_AQU*1e-3), df-F0);
-                if Nfiles>0
-                    corr = GEN_CORR(code);
-                    SIGNAL_TRACKING(df, cst_k, in_sig, prn(x), corr, FILE_NAME, fileNo, t0, Nfiles)
-                end
+                prnFlag = 0;
             end
-        else   
-            fprintf('PRN %02d IGNORED  %s\n', prn(x), string(1:end-2));
         end
-    end
-    fprintf('Elapsed time is %s\n',datestr(datenum(0,0,0,0,0,toc(tStart)),'HH:MM:SS'))
-elseif(choose == 2)
-    sv = LIST_AVAIL_TRACKS();
-    svInp = input(['Enter satellites to obtain navigation solution from. '...
-        'Press enter for [' sprintf('%02u ',sv) ']\n:']);
-    if(~isempty(svInp))
-        sv = svInp;
-    end
-    
-    rPos = input('Please enter position sampling reduction factor (1 => 1000 Hz): ');
-    useIcp = input('Use differential mode? (default no): ');
-    rVel = input('Please enter velocity sampling reduction factor (1 => 1000 Hz): ');
-    
-    % Solve for position.
-    [posHist, gpsTimeHist, recTime, dopHist, resHist]=POSITION_TRACK(sv, rPos, useIcp);
-    
-    % Solve for velocity.
-    [velHist loHist velRecTime velDopHist velResHist] = VELOCITY_TRACK(sv, posHist, recTime, gpsTimeHist, rVel);
         
-    % Save data.
-    save('pos','posHist','gpsTimeHist','recTime','dopHist','resHist',...
-        'velHist','loHist','velRecTime','velDopHist','velResHist');
+        %the number of seconds to track the signal for
+        Nfiles = input('\nEnter the number of seconds to track the signal for: ');
+        
+        fileNo = input('\nEnter which file to start with: ');fprintf('\n');
+        
+        tStart = tic;
+        for x=1:length(prn)
+            % Start by loading the first file.
+            in_sig = LOAD_GPS_DATA(FILE_NAME,fileNo);
+            
+            % Set the time offset of the file. Assume all files have the same
+            % length.
+            t0 = fileNo*length(in_sig)*TP;
+            
+            % Figure out where to place the center frequency. This is used for
+            % acquisition.
+            if USE_AIDING
+                [df, elev] = ESTIMATE_DOPPLER(t0, prn(x));
+                dLO = GET_REC_DATA(t0, 'dFre');
+                F0 = df - dLO;
+                string = sprintf(' Elevation: %+4.f, ',elev);
+            else
+                F0 = 0;
+                elev = [];
+                string = '';
+            end
+            
+            % Do aquisition. The coherent integration time is fixed to 20
+            % milliseconds.
+            if ~USE_AIDING || elev>-20
+                code = CACODEGN(prn(x));
+                corr_aq=DIGITIZE_CA(code,0,N_CODES_AQU*ONE_MSEC_SAM);
+                [df, cst_k, magnitude] = FFT_ACQUISITION(in_sig, corr_aq, F0);
+                
+                % If the signal was not found, quit this satellite.
+                if(CNO(magnitude,N_CODES_AQU*1e-3) < DB_DETECTION)
+                    fprintf('PRN %02d NOT FOUND%s CNo: %2.2f\n', prn(x), string, CNO(magnitude,N_CODES_AQU*1e-3));
+                    % Otherwise, track the satellite.
+                else
+                    fprintf('PRN %02d FOUND    %s CNo: %2.2f, Doppler Frequency: %+3.f\n', prn(x), string, CNO(magnitude,N_CODES_AQU*1e-3), df-F0);
+                    if Nfiles>0
+                        corr = GEN_CORR(code);
+                        SIGNAL_TRACKING(df, cst_k, in_sig, prn(x), corr, FILE_NAME, fileNo, t0, Nfiles)
+                    end
+                end
+            else
+                fprintf('PRN %02d IGNORED  %s\n', prn(x), string(1:end-2));
+            end
+        end
+        fprintf('Elapsed time is %s\n',datestr(datenum(0,0,0,0,0,toc(tStart)),'HH:MM:SS'))
+    case 2
+        sv = LIST_AVAIL_TRACKS();
+        svInp = input(['Enter satellites to obtain navigation solution from or \n'...
+            'press enter for [' sprintf('%02u ',sv) ']\n:']);
+        if(~isempty(svInp))
+            sv = svInp;
+        end
+        
+        disp('')
+        disp('Enter solver controls as a N-by-4 matrix with the following format:')
+        disp('|R1 V1 A1 J1|')
+        disp('|R2 V2 A2 J2|')
+        disp('| .  .  .  .|')
+        disp('|RN VN AN JN|')
+        disp('Each row is one iteration of the solver. R is a downsampling factor.')
+        disp('V, A and J are booleans which enables or disables Doppler, accelerometers')
+        disp('and jerk, respectively.')
+        disp('Press enter to use default:')
+        disp('[1000 0 0 1;...')
+        disp(' 100  1 0 1;...')
+        disp(' 10   1 0 1];')
+        
+        setInp = input(':');
+        if(isempty(setInp))
+            setInp = [1000 0 0 1;...
+                      100  1 0 1;...
+                      10   1 1 1];
+        end
+        
+        GLOBAL_LS_SOL(sv,setInp)
+    case 3
+        sv = LIST_AVAIL_TRACKS();
+        svInp = input(['Enter satellites to obtain navigation solution from or \n'...
+            'press enter for [' sprintf('%02u ',sv) ']\n:']);
+        if(~isempty(svInp))
+            sv = svInp;
+        end
+        
+        rPos = input('Please enter position sampling reduction factor (1 => 1000 Hz): ');
+        useIcp = input('Use differential mode? (default no): ');
+        rVel = input('Please enter velocity sampling reduction factor (1 => 1000 Hz): ');
+        
+        % Solve for position.
+        [posHist, gpsTimeHist, recTime, dopHist, resHist]=POSITION_TRACK(sv, rPos, useIcp);
+        
+        % Solve for velocity.
+        [velHist loHist velRecTime velDopHist velResHist] = VELOCITY_TRACK(sv, posHist, recTime, gpsTimeHist, rVel);
+        
+        % Save data.
+        save('pos','posHist','gpsTimeHist','recTime','dopHist','resHist',...
+            'velHist','loHist','velRecTime','velDopHist','velResHist');
+    otherwise
+        return
 end
