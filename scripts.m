aa6a358319dba029fe569dc9c57fa6960758df39
Erik Lindén <erik.linden42@gmail.com>
2012-07-20 10:25:24 +0200

    Started to make scrips presentable

--- scripts.m
+++ scripts.m
@@ -1,38 +1,44 @@
 %% Local plot
 % dub 3098830.3422 1010993.2627 5463999.7967 ITRF2008
-dub = [3098830.3422 1010993.2627 5463999.7967];
+% dub = [3098830.3422 1010993.2627 5463999.7967];
+CONSTANTS
+
 load(sprintf('%sposLS.mat',TRACK_DIRECTORY))
 w = 1./sum(dopHist(:,1:3).^2,2);
 w(~isfinite(w)) = 0;
 
 pos = nansum(posHist.*repmat(w,1,3))./nansum(w);
 % pos = nanmean(posHist);
-err=(project_vel(pos-dub,dub)-[0,0,1.26]);
-fprintf('Error horizontal: %.2f\n',sqrt(sum(err(1:2).^2)))
-fprintf('Error vertical:   %.2f\n',err(3))
-fprintf('%14f',ecef2lla(pos)')
-fprintf('\n')
 
-% plot(recTime,project_to_surface(posHist,pos))
+lla = ecef2lla(pos);
+fprintf('Mean position:\n')
+fprintf('%.6f%c, %.6f%c, %.1f m\n',lla(1),char(176),lla(2),char(176),lla(3))
 
 posHistProj = project_to_surface(posHist,pos);
 velHistProj = project_vel(velHist,pos);
 
-% plot(velRecTime,velHistProj)
+figure % Position plot
 plot(recTime,posHistProj)
 grid on
 legend('E/W','N/S','Alt.')
 title('GPS position - timeseries')
-xlabel('Time - [s]')
-ylabel('Relative position - [meter]')
+xlabel('Time [sec]')
+ylabel('Relative position [meter]')
 
-addpath('C:\Program Files\MATLAB\R2011a\toolbox\googleearth')
+figure % Velocity plot
+plot(velRecTime,velHistProj)
+grid on
+legend('E/W','N/S','Vert.')
+title('GPS velcoity - timeseries')
+xlabel('Time [sec]')
+ylabel('Velocity [m/s]')
 
-%% Plot in graph and Google Earth
-lla = ecef2lla(posHist(w>0,:));
-dubLla = ecef2lla(dub);
-uvw = project_to_surface(posHist(w>0,:),pos);
-r = 10;  %downsampling factor
+% addpath('C:\Program Files\MATLAB\R2011a\toolbox\googleearth')
+
+%% Top-down plot
+
+uvw = posHistProj;
+r = 1;  %downsampling factor
 
 % ind = (uvw(:,1)<2.9)&(uvw(:,1)>2.5)&(uvw(:,2)<0)&(uvw(:,2)>-.3);
 % [C,I]=max(uvw(:,3));
@@ -42,10 +48,10 @@ r = 10;  %downsampling factor
 ind = 1:r:length(uvw);
 
 scatter(uvw(ind,1),uvw(ind,2),[],uvw(ind,3),'.')
-% grid on
+grid on
 axis equal
-% c = colorbar;
-% ylabel(c,'Altitude [meter]')
+c = colorbar;
+ylabel(c,'Altitude [meter]')
 
 % title('GPS position - local plane')
 xlabel('Relative position E/W [meter]')
@@ -55,26 +61,43 @@ ylabel('Relative position N/S [meter]')
 %   'altitudeMode','clampToGround',...
 %   'iconURL','http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png');
 % kmlStr = ge_plot(lla(:,2),lla(:,1));
-kmlStr = ge_plot3(lla(1:r:end,2),lla(1:r:end,1),lla(1:r:end,3),'altitudeMode','relativeToGround');
-ge_output('ge_output.kml',kmlStr);
+% kmlStr = ge_plot3(lla(1:r:end,2),lla(1:r:end,1),lla(1:r:end,3),'altitudeMode','relativeToGround');
+% ge_output('ge_output.kml',kmlStr);
 
-%%
+%% Velocity magnitude
+r = 10;  %downsampling factor
+s = 100; %smoothing factor
+ind = 1:r:length(velRecTime);
 
-plot(velRecTime(1:1:end),100*smooth(sqrt(sum(velHistProj(1:1:end,1:3).^2,2)),250/5))
+plot(velRecTime(ind),smooth(sqrt(sum(velHistProj(ind,1:3).^2,2)),s))
 axis tight
+title('Velocity')
 xlabel('Time [sec]')
-ylabel('Velocity [cm/s]')
+ylabel('Velocity [m/s]')
 xlim([0,max(velRecTime)])
 
 
-%%
-plot(velRecTime(1:1:end),loHist(1:1:end)/(2*pi))
+%% LO
+r = 10;  %downsampling factor
+s = 100; %smoothing factor
+ind = 1:r:length(velRecTime);
+
+plot(velRecTime(ind),smooth(loHist(ind),s)/(2*pi))
 xlim([0,max(velRecTime)])
+titel(sprintf('LO Frequency smoothed %u samples',s))
 xlabel('Time [sec]')
 ylabel('LO error [Hz]')
 
-%%
-plot(recTime,detrend(gpsTimeHist-recTime))
+%% Clock error
+absTime = gpsTimeHist - min(gpsTimeHist) + min(recTime);
+timeErr = absTime - recTime;
+
+p = polyfit(absTime,timeErr,2);
+
+plot(absTime,(timeErr-polyval(p,absTime))*1e9)
+title(sprintf('Third-order clock error\nDrift is %.1G ns/s and %.1G ns/s^2',p(2)*1e9,p(1)*1e9))
+ylabel('Residual error [ns]')
+xlabel('"True" time [sec]')
 %%
 k = 1000;
 plot(recTime,reshape(smooth(resHist,k),size(resHist)))
